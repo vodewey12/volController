4dc629ca47b77297adeb34aa3883a8d71383cb6c
vodewey12 <voduy12@gmail.com>
2021-06-26 02:48:23 -0400

    updated code with class change

--- volController.py
+++ volController.py
@@ -4,19 +4,44 @@ from ctypes import cast, POINTER, pointer
 from comtypes import CLSCTX_ALL, GUID
 import serial
 
-ser = serial.Serial('COM5', 9600)
-print("connected to: " + ser.portstr)
-count = 1
-lpcguid = pointer(GUID.create_new())
-devices = AudioUtilities.GetSpeakers()
-interface = devices.Activate(
-    IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
-volume = cast(interface, POINTER(IAudioEndpointVolume))
+
+class volController(object):
+
+    def __init__(self, serial_port):
+        self.serial_port = serial_port
+
+        self._stopped = False
+
+    def start(self):
+        ser = serial.Serial(self.serial_port, 9600)
+
+        sessions = AudioUtilities.GetAllSessions()
+        devices = AudioUtilities.GetSpeakers()
+        interface = devices.Activate(
+            IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+        volume = cast(interface, POINTER(IAudioEndpointVolume))
+        while not self._stopped:
+            cc = str(ser.readline())
+            self.changeVolume(volume, float(cc[2:][:-5])/1023.0)
+
+    def changeVolume(self, volume, decibles):
+        volume.SetMasterVolumeLevelScalar(decibles, None)
 
 
-while True:
-    cc = str(ser.readline())
-    volume.SetMasterVolumeLevelScalar((float(cc[2:][:-5])/1023.0), None)
+def attempt_print(s):
+    try:
+        print(s)
+    except:
+        pass
 
 
-ser.close()
+def main():
+    volume_controller = volController('COM5')
+    try:
+        volume_controller.start()
+    finally:
+        attempt_print("Bye!")
+
+
+if __name__ == "__main__":
+    main()

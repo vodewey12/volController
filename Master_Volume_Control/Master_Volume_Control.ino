2eac8a0a948dbef04a9cd6cf45a594aee5b8826a
vodewey12 <voduy12@gmail.com>
2021-06-27 04:01:31 -0400

    adruino file now updated with the latest code

--- Master_Volume_Control/Master_Volume_Control.ino
+++ Master_Volume_Control/Master_Volume_Control.ino
@@ -1,13 +1,38 @@
+int serialValues[2];
 
-
-
-void setup() {
+void setup()
+{
   Serial.begin(9600);
-  
+  pinMode(2, INPUT);
 }
 
-void loop() {
-  int val = analogRead(A0);
-  Serial.println(val);
+void loop()
+{
 
+  updateValues();
+  parseValues();
+}
+
+void updateValues()
+{
+  serialValues[0] = digitalRead(2);
+
+  serialValues[1] = analogRead(A0);
+}
+
+void parseValues()
+{
+  String builtString = String("");
+
+  for (int i = 0; i < 2; i++)
+  {
+    builtString += String((int)serialValues[i]);
+
+    if (i < 1)
+    {
+      builtString += String("|");
+    }
+  }
+
+  Serial.println(builtString);
 }

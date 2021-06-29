51b8aa1ecd4e51948207f072fa823a68560a5a67
vodewey12 <voduy12@gmail.com>
2021-06-29 12:06:53 -0400

    minor bug fixes and formating

--- volController.py
+++ volController.py
@@ -2,8 +2,6 @@ from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
 import serial
 from ctypes import cast, POINTER, pointer
 from comtypes import CLSCTX_ALL, GUID
-import serial
-import sounddevice as sd
 
 
 class volController(object):
@@ -19,26 +17,14 @@ class volController(object):
         self.volume = cast(self.interface, POINTER(IAudioEndpointVolume))
 
     def start(self):
-        self.checkCurrentDevice()
         ser = serial.Serial(self.serial_port, 9600)
         while not self._stopped:
-            '''print(sd.query_devices()[sd.default.device[1]]['name'])
-            if sd.query_devices()[sd.default.device[1]]['name'] != self.currentOutputDevice:
-                self.refreshController()
-                self.currentOutputDevice = sd.query_devices(
-                )[sd.default.device[1]]['name']
-                print("hello")
-                '''
             cc = str(ser.readline())[2:][:-5].split("|")
             button = int(cc[0])
             master_volume = float(cc[1])/1023.0
             if button == 1:
                 self.refreshController()
             self.changeVolume(self.volume, float(master_volume))
-
-    def checkCurrentDevice(self):
-        self.currentOutputDevice = sd.query_devices()[
-            sd.default.device[1]]['name']
 
     def refreshController(self):
         newDevices = AudioUtilities.GetSpeakers()
